749390288c932882698755bce8dc337cb535a44a
SmallShips <2slavrov@gmail.com>
2016-05-25 16:21:33 +0300

    postal theme

---
+++ app/src/main/java/com/blogspot/smallshipsinvest/coolcard/helpers/MyButtonBlue.java
@@ -0,0 +1,56 @@
+package com.blogspot.smallshipsinvest.coolcard.helpers;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+import com.blogspot.smallshipsinvest.coolcard.R;
+
+public class MyButtonBlue extends TextView {
+
+    public int shadow;
+
+    public MyButtonBlue(Context context) {
+        super(context);
+        set(context);
+    }
+
+    public MyButtonBlue(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        set(context);
+    }
+
+    public MyButtonBlue(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        set(context);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean value = super.onTouchEvent(event);
+
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            setBackgroundResource(R.drawable.my_button_blue_normal);
+            setPadding(getPaddingLeft() - shadow, getPaddingTop() - shadow, getPaddingRight() + shadow,
+                    getPaddingBottom() + shadow);
+        } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            setBackgroundResource(R.drawable.my_button_blue_pressed);
+            setPadding(getPaddingLeft() + shadow, getPaddingTop() + shadow, getPaddingRight() - shadow,
+                    getPaddingBottom() - shadow);
+        }
+
+        return value;
+    }
+
+    private void set(Context context) {
+        shadow = context.getResources().getDimensionPixelSize(R.dimen.my_button_shadow_shift);
+        int leftPadding = context.getResources().getDimensionPixelSize(R.dimen.my_button_left_padding);
+        int topPadding = context.getResources().getDimensionPixelSize(R.dimen.my_button_top_padding);
+        setPadding(leftPadding, topPadding, leftPadding + shadow, topPadding + shadow);
+        setBackgroundResource(R.drawable.my_button_blue_normal);
+        setTextColor(ContextCompat.getColor(context, R.color.colorWhite));
+    }
+
+}
